949b40673a5d63a31bc58189b42233e81c2a300b
Diego Mota <diegoalves123@gmail.com>
2019-05-22 22:42:14 -0300

    Section 2: JUnit - 5. Organização dos arquivos de teste

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -1,16 +1,12 @@
 package br.ce.wcaquino.servicos;
 
 import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
-import static org.junit.Assert.assertTrue;
 
 import java.util.Date;
-
-import org.junit.Test;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
-import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoService {
 	
@@ -32,19 +28,5 @@ public class LocacaoService {
 		return locacao;
 	}
 
-	@Test
-	public void teste(){
-		// cenario
-		LocacaoService locacaoService = new LocacaoService();
-		Usuario usuario = new Usuario("Pedro junior");
-		Filme filme = new Filme("Filme 1",2 , 5.0);
-		
-		// acao
-		Locacao locacao = locacaoService.alugarFilme(usuario, filme);
-		
-		//verificacao
-		assertTrue(locacao.getValor() == 2.0);
-		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
-	}
+	
 }
---
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -0,0 +1,42 @@
+package br.ce.wcaquino.servicos;
+
+/****
+ * 
+ * É uma boa prática a classe de teste 'LocacaoServiceTest' ser criada no 
+ * mesmo pacote 'br.ce.wcaquino.servicos'que a classe que será testada 'LocacaoService'.
+ * 
+ * 
+ * 
+ */
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.servicos.LocacaoService;
+import br.ce.wcaquino.utils.DataUtils;
+
+public class LocacaoServiceTest {
+	
+	@Test
+	public void teste(){
+		// cenario
+		LocacaoService locacaoService = new LocacaoService();
+		Usuario usuario = new Usuario("Pedro junior");
+		Filme filme = new Filme("Filme 1",2 , 5.0);
+		
+		// acao
+		Locacao locacao = locacaoService.alugarFilme(usuario, filme);
+		
+		//verificacao
+		assertTrue(locacao.getValor() == 5.0);
+		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+	}
+	
+}
