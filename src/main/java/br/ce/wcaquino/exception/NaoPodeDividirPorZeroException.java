1dd76d2f6c1d60b974c1e6d0c44ea5e07c7613f8
Diego Mota <diegoalves123@gmail.com>
2019-06-16 00:08:07 -0300

    Seção 2: JUnit - 14. TDD, parte 1

---
+++ src/main/java/br/ce/wcaquino/exception/NaoPodeDividirPorZeroException.java
@@ -0,0 +1,7 @@
+package br.ce.wcaquino.exception;
+
+public class NaoPodeDividirPorZeroException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraTest.java
@@ -0,0 +1,71 @@
+package br.ce.wcaquino.servicos;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import br.ce.wcaquino.exception.NaoPodeDividirPorZeroException;
+
+public class CalculadoraTest {
+	
+	private Calculadora calc;
+	
+	@Before
+	public void setUp(){
+		calc = new Calculadora();
+	}
+	
+	@Test
+	public void somarDoisValores() {
+		// cenario
+		int a = 5;
+		int b = 3;
+		
+		// acao
+		int resultado = calc.somar(a , b);
+		
+		//verificacao
+		Assert.assertEquals(8, resultado);
+	}
+	
+	
+
+	@Test
+	public void subtrairDoisValores() {
+		// cenario
+		int a = 8;
+		int b = 5;
+		
+		// acao
+		int resultado = calc.subtrair(a , b);
+		
+		//verificacao
+		Assert.assertEquals(3, resultado);
+	}
+	
+	
+	@Test
+	public void dividirDoisValores() throws NaoPodeDividirPorZeroException {
+		// cenario
+		int a = 6;
+		int b = 3;
+		
+		// acao
+		int resultado = calc.dividir(a , b);
+		
+		//verificacao
+		Assert.assertEquals(2, resultado);
+	}
+	
+	@Test(expected =  NaoPodeDividirPorZeroException.class)
+	public void deveLancarExcecaoDividirPorZero() throws NaoPodeDividirPorZeroException {
+		// cenario
+		int a = 10;
+		int b = 0;
+		
+		// acao
+		calc.dividir(a , b);
+	}
+	
+	
+}
