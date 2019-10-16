d1cfbb6086870bbc9dff2b8b91c76862c3c6ac8c
Diego Mota <diegoalves123@gmail.com>
2019-10-15 21:32:59 -0300

    Seção 6: PowerMock 39. Testando métodos privados diretamente

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -25,6 +25,7 @@ import org.mockito.MockitoAnnotations;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
 
 import br.ce.wcaquino.builders.FilmeBuilder;
 import br.ce.wcaquino.builders.LocacaoBuilder;
@@ -309,5 +310,17 @@ public class LocacaoServiceTest {
 		// verificacao
 		Assert.assertThat(locacao.getValor(), is(1.0));
 		PowerMockito.verifyPrivate(locacaoService).invoke("calcularValorLocacao" , filmes);
+	}
+	
+	@Test
+	public void deveCalcularValorLocacao() throws Exception {
+		//cenario
+		List<Filme> filmes = Arrays.asList(FilmeBuilder.umFilme().agora());
+		
+		//acao
+		Double valor = Whitebox.invokeMethod(locacaoService, "calcularValorLocacao" , filmes);
+		
+		// verificacao
+		Assert.assertThat(valor, is(4.0));
 	}
 }
